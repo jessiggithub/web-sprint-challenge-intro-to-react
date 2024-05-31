29ded21fbc577041f9bc4d75d1f1c9f0d556bddc
Jessica Garcia <jessicagarcia9696@gmail.com>
2024-05-31 18:49:54 -0400

    My first commit

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+
+function Character({ data }) { 
+  const [showHomeworld, setShowHomeworld] = useState(false);
+  
+
+  const toggleHomeworld = () => setShowHomeworld(!showHomeworld);
+
+
+  
+  // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className='character-card' onClick={toggleHomeworld}>
+      <h3 className='character-name'>{data.name}</h3>
+      {showHomeworld && <p>Planet: <span className='character-planet'>{data.homeworld.name}</span></p>}
     </div>
   )
 }
